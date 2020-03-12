e678d8249514200ffd0cb596f85f54bbb697bcd4
colas.mombrun <colas.mombrun@colisweb.com>
2020-03-12 11:42:12 +0100

    fix: increase opentracing version from 0.31.0 to 0.33.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
   }
 
   object OpenTracing {
-    final val opentracing = "0.31.0"
+    final val opentracing = "0.33.0"
     final val api = "io.opentracing" % "opentracing-api" % opentracing
     final val util = "io.opentracing" % "opentracing-util" % opentracing
     final val dd = "com.datadoghq" % "dd-trace-ot" % "0.45.0"
